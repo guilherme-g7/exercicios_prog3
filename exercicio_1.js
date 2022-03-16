3d6d517ab61dce91083a6c527ca10669645aebb5
Guilherme Pereira <guilhermej.p@outlook.com>
2022-03-15 22:16:43 -0300

    exercicio 1

--- exercicio_1.js
+++ exercicio_1.js
@@ -1,3 +1,9 @@
+/* 1) Crie um programa que calcula que le um arquivo
+com 3 valores separados por ‘,’ (a,b,c) que
+representam elementos de uma função de terceiro
+grau. Apresente as raízes. */
+
+
 var input = require('fs').readFileSync('entrada.txt', 'utf8');
 var lines = input.split('\n');
 
